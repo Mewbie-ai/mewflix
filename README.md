cbbc568adc54c6913278fff5ec472026d6368a3d
Michael Jordan <mewbie3@gmail.com>
2022-08-15 09:08:43 +0700

    git init

---
+++ README.md
@@ -0,0 +1,10 @@
+Super Simple Netflix wannabe-clone Web App
+
+How to run:
+
+via npm:
+
+1. on cmd, install node_modules
+   npm install
+2. run on localhost
+   node Server.js
--- public/script.js
+++ public/script.js
@@ -1,8 +1,11 @@
-const API_KEY = "05dd11d19f28344e864c39000bfb940c";
-const API_URL = "https://api.themoviedb.org/3/";
-const discover = "discover/movie";
-function getDiscover(type, header, filter) {
-  const url = `${API_URL}${discover}?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_genres=${type}${filter}`;
+function getDiscover(type, header, filter, page) {
+  const API_KEY = "05dd11d19f28344e864c39000bfb940c";
+  const API_URL = "https://api.themoviedb.org/3/";
+  const discover = "discover/movie";
+  if (!page) {
+    page = Math.floor(Math.random() * 5) + 1;
+  }
+  const url = `${API_URL}${discover}?api_key=${API_KEY}&include_adult=false&include_video=true&page=${page}&with_genres=${type}${filter}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -11,7 +14,8 @@ function getDiscover(type, header, filter) {
       const slider = document.createElement("div");
       slider.classList.add("slider");
       console.log(movies);
-
+      const h2 = document.createElement("h2");
+      h2.innerHTML = header;
       movies.forEach((movie) => {
         const card = document.createElement("div");
         card.classList.add("card");
@@ -20,13 +24,72 @@ function getDiscover(type, header, filter) {
         `;
         slider.appendChild(card);
       });
-
+      main.appendChild(h2);
       main.appendChild(slider);
     });
 }
-getDiscover(28, "Action");
-getDiscover(20, "Horror");
-getDiscover(19, "Popular");
+
+getDiscover(28, "Popular Movies", "&sort_by=popularity.desc", 1);
+getDiscover(11, "Horror Movies");
+getDiscover(19, "Biography Movies");
 getDiscover(18, "Best Dramas");
-getDiscover(22, "Comedy");
-console.log(getDiscover(28));
+getDiscover(35, "Comedy Movies");
+
+function navBurger() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  }),
+    nav.addEventListener("click", () => {
+      nav.classList.remove("nav-active");
+    }),
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 768) {
+        nav.classList.remove("nav-active");
+      }
+    }),
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        nav.classList.add("nav-active");
+      } else {
+        nav.classList.remove("nav-active");
+      }
+    }),
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        burger.classList.add("burger-active");
+      } else {
+        burger.classList.remove("burger-active");
+      }
+    }),
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        nav.classList.add("nav-active");
+      } else {
+        nav.classList.remove("nav-active");
+      }
+    }),
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        nav.classList.add("nav-active");
+      } else {
+        nav.classList.remove("nav-active");
+      }
+    }),
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        nav.classList.add("nav-active");
+      } else {
+        nav.classList.remove("nav-active");
+      }
+    }),
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        nav.classList.add("nav-active");
+      } else {
+        nav.class;
+      }
+    });
+}
+navBurger();
